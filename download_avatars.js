01794e9847762fd50f3fd132d4ee7bb5b19de567
gaber79 <gaber79@users.noreply.github.com>
2016-10-14 12:20:05 -0700

    Update download_avatars.js
    "completed the project to the desired effect"

--- download_avatars.js
+++ download_avatars.js
@@ -1,16 +1,34 @@
-var args = process.argv.slice(2);
+const args = process.argv.slice(2);
 
-var request = require("request");
+const request = require("request");
+const fs = require("fs");
 
-var options = {
-	host: "api.github.com",
-	path: "/users",
-	get: "get"
-};
+function getRepoContributorsImages(repoOwner, repoName, cb) {
 
+	var options = {
+		url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+		headers: {
+			"User-Agent": "request",
+			"Authorization": "token 752bae939266535ac520f00ccd2724988ed8ebc2"
+		}
+	}
 
+	request(options, function(err, response, userData) {
+		if (err) {
+			console.log("It's BROKEN!!! : ", err);
+			return false;
+		}
 
+		var object = JSON.parse(userData);
 
+		object.forEach(function(avatar) {
+			var avatarPath = avatar["avatar_url"];
+			var user = avatar["login"];
+			request(avatarPath).pipe(fs.createWriteStream("avatars/" + user + ".png"));
+		})
 
+		console.log("The pictures were stored in avatarImages folder.")
+	})
+}
 
-request.
+getRepoContributorsImages(args[0], args[1]);
